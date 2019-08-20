bdc128101452dd958229e1c853defc88b660a273
DCG <53319568+expungebob@users.noreply.github.com>
2019-08-20 16:54:15 -0700

    JavaScript push1

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,22 @@
+
+var A ="this is a string";
+window.alert(A);
+
+document.write("\"Be who you are and say what you feel,"
++"because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr. Seuss ");
+
+document.write(" Lisa told Bart, \"Why do you eat shorts all the time? would you like to see a therapist\" <br> \"Yes I need help \" Bart responded. ")
+
+var Brand = "Sony", Type= "camera", Model="Alpha7R "
+var Model = Model.fontcolor("blue");
+var Brand = Brand.fontcolor("blue");
+var Type = Type.fontcolor("blue");
+document.write(Model)
+
+document.write(4*3);
+
+var string = ' There once '; 
+var value = string.concat('was ',' a ','man',' from ',' Nantucket '); 
+document.write(value);	 
+
