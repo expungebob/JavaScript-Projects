ea509c4a75ecf26e84ec3453d7e687e516fb5b42
DCG <53319568+expungebob@users.noreply.github.com>
2019-08-21 10:31:38 -0700

    HTML-3

---
+++ JS/Basic_JavaScript_3.js
@@ -0,0 +1,22 @@
+
+function function_1() {
+    var result_add = 9+10;
+    document.getElementById("add").innerHTML = result_add;
+  }
+  function function_2() {
+    var result_sub = 7-6;
+    document.getElementById("sub").innerHTML = result_sub;
+  }
+  function function_3() {
+    var result_mult = 12*12;
+    document.getElementById("mult").innerHTML = result_mult;
+  }
+  function function_4() {
+    var result_div = 144/12;
+    document.getElementById("div").innerHTML = result_div;
+  }
+  function function_5() {
+    var maths = ((14+8)*6)/4;
+    document.getElementById("maths").innerHTML = maths;
+  }
+  
