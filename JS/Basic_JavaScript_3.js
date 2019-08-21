87a2e2f34edd3417dc8156cb3ffc448589be5ac2
DCG <53319568+expungebob@users.noreply.github.com>
2019-08-21 11:29:46 -0700

    JS-3
    Step-85

--- JS/Basic_JavaScript_3.js
+++ JS/Basic_JavaScript_3.js
@@ -19,4 +19,29 @@ function function_1() {
     var maths = ((14+8)*6)/4;
     document.getElementById("maths").innerHTML = maths;
   }
+  function function_6() {
+    var modulus = 113/16;
+    document.getElementById("modulus").innerHTML = "The remainder of 113 divided by 16 is: " +modulus;
+  }
+  function function_7() {
+    var unary = 1024;
+    document.getElementById("unary").innerHTML = -unary;
+  }
+  function function_8() {
+    var x = 3.14156;
+    x++;
+    document.getElementById("increment").innerHTML = x;
+  }
+  function function_9() {
+    var y = 100
+    y--;
+    document.getElementById("decrement").innerHTML = y;
+  }
+  window.alert(Math.random());
+  window.alert(Math.random()*100);
+
+  function function_10() {
+    var Maths_2 = Math.E;
+    document.getElementById("Maths_2").innerHTML= Maths_2;
+  }
   
